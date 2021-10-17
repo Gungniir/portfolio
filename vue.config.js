8dc2ad1bfd3670e5da0d843ccd333c45a04d0c30
gungniir <vladimir.kurus@yandex.ru>
2021-10-17 21:47:05 +0700

    Иконка и название сайта

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pages: {
+    index: {
+      title: "Портфолио",
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+  }
 }
